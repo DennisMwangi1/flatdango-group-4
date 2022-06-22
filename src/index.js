6e23ae6298ac808afff35b4c80288b7022bbb6b7
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-22 11:01:32 +0300

    got elements from the DOM  and assigned them to variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded',()=>{
-
+    const movieTitles = document.getElementById("films")
+    const moviePoster = document.getElementById("poster")
+    const movieTitle = document.getElementById("title")
+    const runTime = document.getElementById("runtime")
+    const movieInfo = document.getElementById("film-info")
+    const showTime = document.getElementById('showtime')
+    const ticketNum = document.getElementById("ticket-num")
+    const buyBtn = document.getElementById("buy-ticket")
 
 
 
