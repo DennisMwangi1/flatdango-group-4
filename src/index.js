b8dc47d3259ff06f81f9e868f83bee514c28f9a4
rickie maweu <rickiemaweu@gmail.com>
2022-06-22 12:46:56 +0300

    testing commits

--- src/index.js
+++ src/index.js
@@ -18,3 +18,28 @@ const movieDescription=document.getElementById('film-info');
 const movieShowTime=document.getElementById('showtime');
 const movieTickets=document.getElementById('ticket-num');
 const moviePoster=document.getElementById('poster');
+
+function renderMovies(movies){
+    movies.forEach(movies=>{
+        let list =document.createElement('li');
+        list.innerText= movies.title;
+        movieList.appendChild(list);
+        
+        //render spefic movie details
+        list.addEventListener('click',()=>{
+            movieTitle.textContent=movies.title;
+            movieRuntime.textContent=movies.runtime;
+            movieDescription.textContent=movies.description;
+            movieShowTime.textContent=movies.showtime;
+            moviePoster.src=movies.poster;
+            //tickets remaining
+            let capacity=movies.capacity;
+            let soldTickets=movies.tickets_sold;
+            movieTickets.textContent=(capacity-soldTickets);
+
+
+        })
+
+    })
+
+}
