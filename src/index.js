46ce70b6425a6cea8df29001a50dc29313899dd2
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-22 11:07:18 +0300

    used GET method to fetch data from the json server and render the first film in the DOM

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ document.addEventListener('DOMContentLoaded',()=>{
     const ticketNum = document.getElementById("ticket-num")
     const buyBtn = document.getElementById("buy-ticket")
 
+    fetch('http://localhost:3000/films')
+    .then((res)=>res.json())
+    .then((data)=>{
+        moviePoster.src = data[0].poster
+        movieTitle.innerText = data[0].title
+        runTime.innerText = `${data[0].runtime} minutes`
+        movieInfo.innerText = data[0].description
+        showTime.innerText = data[0].showtime
+        ticketNum.innerText =data[0].tickets_sold
+        let tickets = data[0].tickets_sold
+    })
+
 
 
 
