098d81f029db0ccce6f7a44110315a84c866fd15
rickie maweu <rickiemaweu@gmail.com>
2022-06-22 12:40:46 +0300

    testing commits

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchData();
   });
 
+
 //fetch data function
 function fetchData(){
     fetch('http://localhost:3000/films')
     .then(response => response.json())
     .then((movies)=>renderMovies(movies))
 }
+//render moviees function...
+//deg dom contents
+const movieList= document.querySelector('#films');
+const movieTitle=document.getElementById('title');
+const movieRuntime=document.getElementById('runtime');
+const movieDescription=document.getElementById('film-info');
+const movieShowTime=document.getElementById('showtime');
+const movieTickets=document.getElementById('ticket-num');
+const moviePoster=document.getElementById('poster');
